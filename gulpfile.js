d779fdb58a48328c9fc69ffa226cd67c7b8d630a
James White <james@jmwhite.co.uk>
2016-07-21 23:23:21 +0100

    moar things

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,22 @@ gulp.task('default', function() {
     var preprocess_template_file = 'app/email-boilerplate-preprocess.html';
     gulp.src(preprocess_template_file)
     .pipe(preprocess(
-    	{
-    		context: 
-    			{
+        {
+            context: 
+                {
                     // 
                     // Pre-processing variables for customisation of email boilerplate template
-    				//
+                    //
 
                     // DOCTYPE, HEAD AND META
                     DOCTYPE_VERSION: 'XHTML_TRANSITIONAL',
                     DOCUMENT_TITLE: 'Central College Nottingham',
-    				ENABLE_FAVICON: true,
+                    ENABLE_FAVICON: true,
                     FAVICON_URL: 'http://media.centralnottingham.ac.uk/favicon.ico',
 
                     // MSO CONDITIONAL CODE
                     ENABLE_MSO_DPI_SCALING_FIX: true,
+                    ENABLE_MSO_FALLBACK_FONT: true,
                     MSO_DEFAULT_FALLBACK_FONT_STACK: 'Helvetica, Arial, sans-serif',
 
                     // CSS CONFIGURATION IN THE HEAD
@@ -30,6 +31,7 @@ gulp.task('default', function() {
                     DEFAULT_ANCHOR_VISITED_COLOUR: '#ff0000',
                     ENABLE_VISITED_ANCHOR_CSS: false, // Not all email clients support this styling
                     ENABLE_HEADER_SELECTORS_CSS: false,
+                    ENABLE_YAHOO_SHORTCUT_CSS_CLASSES: false,
 
                     // CSS3 MEDIA QUERIES
                     ENABLE_MEDIA_SCREEN_QUERY: true,
@@ -44,9 +46,9 @@ gulp.task('default', function() {
 
                     // MISC
                     PLACEHOLDER_IMG_URL: 'http://placehold.it'
-    			}
-    		}
-    	))
+                }
+            }
+        ))
     .pipe(rename("email-boilerplate-without-guidelines.html"))
     .pipe(gulp.dest('./dist/'))
 
